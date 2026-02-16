ac3b4188e6e80f605e6f340df0526225eed25c3c
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-16 20:19:26 +0600

    feat(category): add category list table component

--- resources/views/pages/dashboard/category-page.blade.php
+++ resources/views/pages/dashboard/category-page.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.sidenav-layout')
 @section('content')
+    @include('components.category.category-list')
     @include('components.category.category-delete')
     @include('components.category.category-create')
     @include('components.category.category-update')

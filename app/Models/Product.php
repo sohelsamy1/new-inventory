f265fa09d7bad0ee0c23cbbfe37d1dde9d9e1d34
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-19 17:31:04 +0600

    feat: create Product model and define fillable properties

---
+++ app/Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+      protected $fillable = [
+        'name',
+        'price',
+        'unit',
+        'img_url',
+        'user_id',
+        'category_id'
+
+    ];
+}

7b3b5aae1e2ef08a5ff1032951af8df1bb76bdd3
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-19 22:27:11 +0600

    feat: create Customer model with fillable properties

---
+++ app/Models/Customer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+
+   protected $fillable = [
+        'name',
+        'email',
+        'mobile',
+        'user_id'
+
+    ];
+
+}

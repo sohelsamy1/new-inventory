7ca3061747e0ca78c8272547bec3c482dc7e0309
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-20 16:23:58 +0600

    feat: add user registration API with validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -28,5 +30,47 @@ class UserController extends Controller
 
       function profilePage(){
         return view('pages.dashboard.profile-page');
+    }
+
+      // Registration
+    public function userRegistration(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'firstName' => 'required|string|max:255',
+                'lastName' => 'required|string|max:255',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|min:3',
+                'mobile' => 'required|string|max:15'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'fail',
+                    'message' => 'Validation Error',
+                    'errors' => $validator->errors()
+                ], 422);
+            }
+
+            User::create([
+                'first_name' => $request->firstName,
+                'last_name' => $request->lastName,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+                'mobile' => $request->mobile,
+            ]);
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User Registration Successful'
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'User Registration Failed',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 }

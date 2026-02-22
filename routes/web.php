09ec18a727a35e5c01404bfd647a2f7c23766937
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-22 19:08:06 +0600

    feat: add CustomerController and web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
@@ -18,6 +19,7 @@ Route::get('/resetPassword', [UserController::class, 'resetPasswordPage']);
 Route::get('/sendOtp', [UserController::class, 'sendOtpPage']);
 Route::get('/verifyOtp', [UserController::class, 'verifyOtpPage']);
 Route::get('/userProfile', [UserController::class, 'profilePage']);
+Route::get('/customerPage',[CustomerController::class,'CustomerPage'])->name('CustomerPage')->middleware([TokenVerificationMiddleware::class]);
 
 // API
 Route::post('/user-registration', [UserController::class, 'userRegistration']);

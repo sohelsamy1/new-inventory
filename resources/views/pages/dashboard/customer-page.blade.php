c636a1e1fe0f276bd5439981607c4bd3a3104088
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-22 20:57:22 +0600

    feat: add customer update modal and form UI

--- resources/views/pages/dashboard/customer-page.blade.php
+++ resources/views/pages/dashboard/customer-page.blade.php
@@ -3,4 +3,5 @@
      @include('components.customer.customer-create')
      @include('components.customer.customer-delete')
      @include('components.customer.customer-list')
+     @include('components.customer.customer-update')
 @endsection

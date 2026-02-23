dbbf61682c814b7f40ef3e574e8a20c120e78255
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-23 20:38:22 +0600

    feat: add product update modal with image preview

--- resources/views/pages/dashboard/product-page.blade.php
+++ resources/views/pages/dashboard/product-page.blade.php
@@ -3,4 +3,5 @@
     @include('components.product.product-create')
     @include('components.product.product-delete')
     @include('components.product.product-list')
+    @include('components.product.product-update')
 @endsection

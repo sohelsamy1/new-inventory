8e9f52576923a093c7d1fedc6dd6c801085b79fc
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-25 00:04:25 +0600

    feat: add invoice delete modal UI

--- resources/views/pages/dashboard/invoice-page.blade.php
+++ resources/views/pages/dashboard/invoice-page.blade.php
@@ -2,4 +2,5 @@
 @section('content')
     @include('components.invoice.invoice-list')
     @include('components.invoice.invoice-details')
-@endsection
+    @include('components.invoice.invoice-delete')
+    @endsection

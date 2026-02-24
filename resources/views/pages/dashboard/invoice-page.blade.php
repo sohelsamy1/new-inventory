4e66188d3d09efeb70dd86c1547932902dc60092
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-24 22:50:03 +0600

    feat: add invoice list page UI with controller and routes

---
+++ resources/views/pages/dashboard/invoice-page.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.sidenav-layout')
+@section('content')
+    @include('components.invoice.invoice-list')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,12 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\TokenVerificationMiddleware;
 use Illuminate\Support\Facades\Route;
+
 
 
 // Frontend Routes
@@ -22,6 +24,7 @@ Route::get('/verifyOtp', [UserController::class, 'verifyOtpPage']);
 Route::get('/userProfile', [UserController::class, 'profilePage']);
 Route::get('/customerPage',[CustomerController::class,'CustomerPage'])->name('CustomerPage');
 Route::get('/productPage',[ProductController::class,'ProductPage']);
+Route::get('/invoicePage',[InvoiceController::class,'InvoicePage']);
 
 // API
 Route::post('/user-registration', [UserController::class, 'userRegistration']);

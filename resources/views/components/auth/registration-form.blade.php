5830db6d5337c8e82c048a8650aec049362448a9
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-26 23:49:50 +0600

    feat: implement user registration with axios and validation handling

--- resources/views/components/auth/registration-form.blade.php
+++ resources/views/components/auth/registration-form.blade.php
@@ -40,3 +40,61 @@
     </div>
 </div>
 
+<script>
+    async function onRegistration() {
+        let email = document.getElementById('email').value;
+        let firstName = document.getElementById('firstName').value;
+        let lastName = document.getElementById('lastName').value;
+        let mobile = document.getElementById('mobile').value;
+        let password = document.getElementById('password').value;
+
+        if (email.length === 0) {
+            errorToast('Email is required');
+        } else if (firstName.length === 0) {
+            errorToast('First Name is required');
+        } else if (lastName.length === 0) {
+            errorToast('Last Name is required');
+        } else if (mobile.length === 0) {
+            errorToast('Mobile is required');
+        } else if (password.length === 0) {
+            errorToast('Password is required');
+        } else {
+            showLoader();
+            try {
+                let res = await axios.post("/user-registration", {
+                    email: email,
+                    firstName: firstName,
+                    lastName: lastName,
+                    mobile: mobile,
+                    password: password
+                });
+
+                hideLoader();
+
+                if (res.status === 200 && res.data.status === 'success') {
+                    successToast(res.data.message);
+                    setTimeout(function () {
+                        window.location.href = '/userLogin';
+                    }, 2000);
+                } else {
+                    errorToast(res.data.message);
+                }
+
+            } catch (err) {
+                hideLoader();
+                if (err.response && err.response.status === 422) {
+                    // Show each validation error
+                    let errors = err.response.data.errors;
+                    for (let field in errors) {
+                        if (errors.hasOwnProperty(field)) {
+                            errorToast(errors[field][0]);
+                        }
+                    }
+                } else {
+                    errorToast("An unexpected error occurred");
+                }
+            }
+        }
+    }
+</script>
+

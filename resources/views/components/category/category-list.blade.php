0c0081ca569874819e3f1ab46c3b28489907bcf8
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-27 23:16:00 +0600

    feat: add AJAX script for fetching and displaying category list with DataTable

--- resources/views/components/category/category-list.blade.php
+++ resources/views/components/category/category-list.blade.php
@@ -29,3 +29,55 @@
     </div>
 </div>
 </div>
+
+
+<script>
+
+getList();
+
+async function getList() {
+
+    showLoader();
+    let res=await axios.get("/list-category");
+    hideLoader();
+
+    let tableList=$("#tableList");
+    let tableData=$("#tableData");
+
+    tableData.DataTable().destroy();
+    tableList.empty();
+
+    res.data.forEach(function (item,index) {
+        let row=`<tr>
+                    <td>${index+1}</td>
+                    <td>${item['name']}</td>
+                    <td>
+                        <button data-id="${item['id']}" class="btn editBtn btn-sm btn-outline-success">Edit</button>
+                        <button data-id="${item['id']}" class="btn deleteBtn btn-sm btn-outline-danger">Delete</button>
+                    </td>
+                 </tr>`
+        tableList.append(row)
+    })
+
+    $('.editBtn').on('click', async function () {
+           let id= $(this).data('id');
+           await FillUpUpdateForm(id)
+           $("#update-modal").modal('show');
+
+    })
+
+    $('.deleteBtn').on('click',function () {
+        let id= $(this).data('id');
+        $("#delete-modal").modal('show');
+        $("#deleteID").val(id);
+    })
+
+    new DataTable('#tableData',{
+       order:[[0,'desc']],
+       lengthMenu:[5,10,15,20,30]
+   });
+
+}
+
+</script>
+

151aa52adff1c517be807c4b92c279a3339554bb
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-27 14:01:07 +0600

    feat: add ResetPassword function with axios password reset

--- resources/views/components/auth/reset-pass-form.blade.php
+++ resources/views/components/auth/reset-pass-form.blade.php
@@ -17,3 +17,50 @@
         </div>
     </div>
 </div>
+
+<script>
+    async function ResetPassword() {
+    let password = document.getElementById('password').value;
+    let cpassword = document.getElementById('cpassword').value;
+
+    if (password.length === 0) {
+        errorToast('Password is required');
+    } else if (cpassword.length === 0) {
+        errorToast('Confirm Password is required');
+    } else if (password !== cpassword) {
+        errorToast('Password and Confirm Password must be same');
+    } else {
+        showLoader();
+        try {
+            let res = await axios.post('/reset-password', {
+                password: password,
+                password_confirmation: cpassword
+            });
+
+            hideLoader();
+
+            if (res.status === 200 && res.data.status === 'success') {
+                successToast(res.data.message);
+                setTimeout(() => {
+                    window.location.href = '/userLogin';
+                }, 1000);
+            } else {
+                errorToast(res.data.message);
+            }
+
+        } catch (err) {
+            hideLoader();
+            if (err.response && err.response.status === 422) {
+                let errors = err.response.data.errors;
+                for (let field in errors) {
+                        if (errors.hasOwnProperty(field)) {
+                            errorToast(errors[field][0]);
+                        }
+                 }
+            } else {
+                errorToast("Something went wrong");
+            }
+        }
+    }
+}
+</script>

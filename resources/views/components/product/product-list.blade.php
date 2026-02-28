b985f6647b99e8f79cb5dd7db8d2789a09fb0f69
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-28 21:12:58 +0600

    feat: add product list with edit and delete actions using axios

--- resources/views/components/product/product-list.blade.php
+++ resources/views/components/product/product-list.blade.php
@@ -31,4 +31,62 @@
 </div>
 
 
+<script>
+
+ getList();
+
+ async function getList() {
+
+
+    showLoader();
+    let res=await axios.get("/list-product");
+    hideLoader();
+
+    let tableList=$("#tableList");
+    let tableData=$("#tableData");
+
+    tableData.DataTable().destroy();
+    tableList.empty();
+
+    res.data.forEach(function (item,index) {
+        let row=`<tr>
+                    <td><img class="w-15 h-auto" alt="" src="${item['img_url']}"></td>
+                    <td>${item['name']}</td>
+                    <td>${item['price']}</td>
+                    <td>${item['unit']}</td>
+                    <td>
+                        <button data-path="${item['img_url']}" data-id="${item['id']}" class="btn editBtn btn-sm btn-outline-success">Edit</button>
+                        <button data-path="${item['img_url']}" data-id="${item['id']}" class="btn deleteBtn btn-sm btn-outline-danger">Delete</button>
+                    </td>
+                 </tr>`
+        tableList.append(row)
+    })
+
+    $('.editBtn').on('click', async function () {
+           let id= $(this).data('id');
+           let filePath= $(this).data('path');
+           await FillUpUpdateForm(id,filePath)
+           $("#update-modal").modal('show');
+    })
+
+    $('.deleteBtn').on('click',function () {
+        let id= $(this).data('id');
+        let path= $(this).data('path');
+
+        $("#delete-modal").modal('show');
+        $("#deleteID").val(id);
+        $("#deleteFilePath").val(path)
+
+    })
+
+    new DataTable('#tableData',{
+        order:[[0,'desc']],
+        lengthMenu:[5,10,15,20,30]
+    });
+
+ }
+
+
+</script>
+
 

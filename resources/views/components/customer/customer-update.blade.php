6e069e33f3b4a409cf954de22a494b544b0aaf27
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-28 20:13:36 +0600

    feat: implement customer update functionality

--- resources/views/components/customer/customer-update.blade.php
+++ resources/views/components/customer/customer-update.blade.php
@@ -33,3 +33,56 @@
 </div>
 
 
+<script>
+
+    async function FillUpUpdateForm(id){
+        document.getElementById('updateID').value=id;
+        showLoader();
+        let res=await axios.post("/customer-by-id",{id:id})
+        hideLoader();
+        document.getElementById('customerNameUpdate').value=res.data['name'];
+        document.getElementById('customerEmailUpdate').value=res.data['email'];
+        document.getElementById('customerMobileUpdate').value=res.data['mobile'];
+    }
+
+    async function Update() {
+
+        let customerName = document.getElementById('customerNameUpdate').value;
+        let customerEmail = document.getElementById('customerEmailUpdate').value;
+        let customerMobile = document.getElementById('customerMobileUpdate').value;
+        let updateID = document.getElementById('updateID').value;
+
+        if (customerName.length === 0) {
+            errorToast("Customer Name Required !")
+        }
+        else if(customerEmail.length===0){
+            errorToast("Customer Email Required !")
+        }
+        else if(customerMobile.length===0){
+            errorToast("Customer Mobile Required !")
+        }
+        else {
+
+            document.getElementById('update-modal-close').click();
+
+            showLoader();
+
+            let res = await axios.post("/update-customer",{name:customerName,email:customerEmail,mobile:customerMobile,id:updateID})
+
+            hideLoader();
+
+            if(res.status===200 && res.data===1){
+
+                successToast('Request completed');
+
+                document.getElementById("update-form").reset();
+
+                await getList();
+            }
+            else{
+                errorToast("Request fail !")
+            }
+        }
+    }
+
+</script>

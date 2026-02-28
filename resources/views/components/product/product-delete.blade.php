ad99035e38040187c070ca5d1d236d36e97d89b6
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-03-01 00:26:29 +0600

    feat: add product delete with axios and image delete support

--- resources/views/components/product/product-delete.blade.php
+++ resources/views/components/product/product-delete.blade.php
@@ -19,3 +19,20 @@
 </div>
 
 
+<script>
+     async function itemDelete(){
+            let id=document.getElementById('deleteID').value;
+            let deleteFilePath=document.getElementById('deleteFilePath').value;
+            document.getElementById('delete-modal-close').click();
+            showLoader();
+            let res=await axios.post("/delete-product",{id:id,file_path:deleteFilePath})
+            hideLoader();
+            if(res.data===1){
+                successToast("Request completed")
+                await getList();
+            }
+            else{
+                errorToast("Request fail!")
+            }
+     }
+</script>

5ce0fa2b3d860d99d29db3a6d47bd729869c3b21
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-28 19:14:52 +0600

    feat: add customer delete functionality

--- resources/views/components/customer/customer-delete.blade.php
+++ resources/views/components/customer/customer-delete.blade.php
@@ -17,3 +17,19 @@
     </div>
 </div>
 
+<script>
+     async function itemDelete(){
+            let id=document.getElementById('deleteID').value;
+            document.getElementById('delete-modal-close').click();
+            showLoader();
+            let res=await axios.post("/delete-customer",{id:id})
+            hideLoader();
+            if(res.data===1){
+                successToast("Request completed")
+                await getList();
+            }
+            else{
+                errorToast("Request fail!")
+            }
+        }
+</script>

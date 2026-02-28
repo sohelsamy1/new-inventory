770c00e74427d0cdcd7dbd5bc2855c777c1d9be1
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-02-28 23:18:49 +0600

    feat: add product create with axios api integration and image upload

--- resources/views/components/product/product-create.blade.php
+++ resources/views/components/product/product-create.blade.php
@@ -44,3 +44,72 @@
     </div>
 </div>
 
+<script>
+
+    FillCategoryDropDown();
+
+    async function FillCategoryDropDown(){
+        let res = await axios.get("/list-category")
+        res.data.forEach(function (item,i) {
+            let option=`<option value="${item['id']}">${item['name']}</option>`
+            $("#productCategory").append(option);
+        })
+    }
+
+    async function Save() {
+
+        let productCategory=document.getElementById('productCategory').value;
+        let productName = document.getElementById('productName').value;
+        let productPrice = document.getElementById('productPrice').value;
+        let productUnit = document.getElementById('productUnit').value;
+        let productImg = document.getElementById('productImg').files[0];
+
+        if (productCategory.length === 0) {
+            errorToast("Product Category Required !")
+        }
+        else if(productName.length===0){
+            errorToast("Product Name Required !")
+        }
+        else if(productPrice.length===0){
+            errorToast("Product Price Required !")
+        }
+        else if(productUnit.length===0){
+            errorToast("Product Unit Required !")
+        }
+        else if(!productImg){
+            errorToast("Product Image Required !")
+        }
+
+        else {
+
+            document.getElementById('modal-close').click();
+
+            let formData=new FormData();
+            formData.append('img',productImg)
+            formData.append('name',productName)
+            formData.append('price',productPrice)
+            formData.append('unit',productUnit)
+            formData.append('category_id',productCategory)
+
+            const config = {
+                headers: {
+                    'content-type': 'multipart/form-data'
+                }
+            }
+
+            showLoader();
+            let res = await axios.post("/create-product",formData,config)
+            hideLoader();
+
+            if(res.status===201){
+                successToast('Request completed');
+                document.getElementById("save-form").reset();
+                await getList();
+            }
+            else{
+                errorToast("Request fail !")
+            }
+        }
+    }
+    
+</script>

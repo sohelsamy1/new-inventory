59c4a2f5198a19112f648be897822be16940e400
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-03-01 09:22:14 +0600

    feat: add product update functionality with axios and image upload

--- resources/views/components/product/product-update.blade.php
+++ resources/views/components/product/product-update.blade.php
@@ -10,7 +10,7 @@
                         <div class="row">
                             <div class="col-12 p-1">
 
-                                
+
                                 <label class="form-label">Category</label>
                                 <select type="text" class="form-control form-select" id="productCategoryUpdate">
                                     <option value="">Select Category</option>
@@ -50,4 +50,90 @@
 </div>
 
 
+<script>
 
+    async function UpdateFillCategoryDropDown(){
+        let res = await axios.get("/list-category")
+        res.data.forEach(function (item,i) {
+            let option=`<option value="${item['id']}">${item['name']}</option>`
+            $("#productCategoryUpdate").append(option);
+        })
+    }
+
+    async function FillUpUpdateForm(id,filePath){
+
+        document.getElementById('updateID').value=id;
+        document.getElementById('filePath').value=filePath;
+        document.getElementById('oldImg').src=filePath;
+
+        showLoader();
+        await UpdateFillCategoryDropDown();
+
+        let res=await axios.post("/product-by-id",{id:id})
+        hideLoader();
+
+        document.getElementById('productNameUpdate').value=res.data['name'];
+        document.getElementById('productPriceUpdate').value=res.data['price'];
+        document.getElementById('productUnitUpdate').value=res.data['unit'];
+        document.getElementById('productCategoryUpdate').value=res.data['category_id'];
+
+    }
+
+    async function update() {
+
+        let productCategoryUpdate=document.getElementById('productCategoryUpdate').value;
+        let productNameUpdate = document.getElementById('productNameUpdate').value;
+        let productPriceUpdate = document.getElementById('productPriceUpdate').value;
+        let productUnitUpdate = document.getElementById('productUnitUpdate').value;
+        let updateID=document.getElementById('updateID').value;
+        let filePath=document.getElementById('filePath').value;
+        let productImgUpdate = document.getElementById('productImgUpdate').files[0];
+
+        if (productCategoryUpdate.length === 0) {
+            errorToast("Product Category Required !")
+        }
+        else if(productNameUpdate.length===0){
+            errorToast("Product Name Required !")
+        }
+        else if(productPriceUpdate.length===0){
+            errorToast("Product Price Required !")
+        }
+        else if(productUnitUpdate.length===0){
+            errorToast("Product Unit Required !")
+        }
+
+        else {
+
+            document.getElementById('update-modal-close').click();
+
+            let formData=new FormData();
+            formData.append('img',productImgUpdate)
+            formData.append('id',updateID)
+            formData.append('name',productNameUpdate)
+            formData.append('price',productPriceUpdate)
+            formData.append('unit',productNameUpdate)
+            formData.append('category_id',productCategoryUpdate)
+            formData.append('file_path',filePath)
+
+            const config = {
+                headers: {
+                    'content-type': 'multipart/form-data'
+                }
+            }
+
+            showLoader();
+            let res = await axios.post("/update-product",formData,config)
+            hideLoader();
+
+            if(res.status===200 && res.data===1){
+                successToast('Request completed');
+                document.getElementById("update-form").reset();
+                await getList();
+            }
+            else{
+                errorToast("Request fail !")
+            }
+        }
+    }
+    
+</script>

36a80657541f672cd381582fc2278e849915e8a6
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-03-01 15:32:08 +0600

    feat: add invoice list with axios integration

--- resources/views/components/invoice/invoice-list.blade.php
+++ resources/views/components/invoice/invoice-list.blade.php
@@ -34,3 +34,57 @@
 </div>
 
 
+<script>
+
+ getList();
+
+ async function getList() {
+
+    showLoader();
+    let res=await axios.get("/invoice-select");
+    hideLoader();
+
+    let tableList=$("#tableList");
+    let tableData=$("#tableData");
+
+    tableData.DataTable().destroy();
+    tableList.empty();
+
+    res.data.forEach(function (item,index) {
+        let row=`<tr>
+                    <td>${index+1}</td>
+                    <td>${item['customer']['name']}</td>
+                    <td>${item['customer']['mobile']}</td>
+                    <td>${item['total']}</td>
+                    <td>${item['vat']}</td>
+                    <td>${item['discount']}</td>
+                    <td>${item['payable']}</td>
+                    <td>
+                        <button data-id="${item['id']}" data-cus="${item['customer']['id']}" class="viewBtn btn btn-outline-dark text-sm px-3 py-1 btn-sm m-0"><i class="fa text-sm fa-eye"></i></button>
+                        <button data-id="${item['id']}" data-cus="${item['customer']['id']}" class="deleteBtn btn btn-outline-dark text-sm px-3 py-1 btn-sm m-0"><i class="fa text-sm  fa-trash-alt"></i></button>
+                    </td>
+                 </tr>`
+        tableList.append(row)
+    })
+
+    $('.viewBtn').on('click', async function () {
+        let id= $(this).data('id');
+        let cus= $(this).data('cus');
+        await InvoiceDetails(cus,id)
+    })
+
+    $('.deleteBtn').on('click',function () {
+        let id= $(this).data('id');
+        document.getElementById('deleteID').value=id;
+        $("#delete-modal").modal('show');
+    })
+
+    new DataTable('#tableData',{
+        order:[[0,'desc']],
+        lengthMenu:[5,10,15,20,30]
+    });
+
+ }
+
+</script>
+

b1da4c772041d982b44eaffd63ca82fcdbf87218
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-03-01 17:18:12 +0600

    feat: add invoice delete with axios integration

--- resources/views/components/invoice/invoice-delete.blade.php
+++ resources/views/components/invoice/invoice-delete.blade.php
@@ -16,3 +16,21 @@
     </div>
 </div>
 
+
+
+<script>
+     async function itemDelete(){
+            let id=document.getElementById('deleteID').value;
+            document.getElementById('delete-modal-close').click();
+            showLoader();
+            let res=await axios.post("/invoice-delete",{inv_id:id})
+            hideLoader();
+            if(res.data===1){
+                successToast("Request completed")
+                await getList();
+            }
+            else{
+                errorToast("Request fail!")
+            }
+     }
+</script>

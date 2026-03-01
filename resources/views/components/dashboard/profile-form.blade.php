81eb17ca6470a868bf9b41a719342c248a77c8ca
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-03-01 11:20:43 +0600

    feat: add user profile view and update functionality using axios

--- resources/views/components/dashboard/profile-form.blade.php
+++ resources/views/components/dashboard/profile-form.blade.php
@@ -41,3 +41,60 @@
 </div>
 
 
+<script>
+    getProfile()
+    async function getProfile(){
+        showLoader();
+        let res=await axios.get("/user-profile")
+        hideLoader();
+        if(res.status===200 && res.data['status']==='success'){
+            // console.log(res.data.data);
+            let data=res.data['data'];
+            document.getElementById('email').value=data['email'];
+            document.getElementById('firstName').value=data['first_name'];
+            document.getElementById('lastName').value=data['last_name'];
+            document.getElementById('mobile').value=data['mobile'];
+            document.getElementById('password').value=data['password'];
+        }
+        else{
+            errorToast(res.data['message'])
+        }
+    }
+
+    async function onUpdate() {
+        let firstName = document.getElementById('firstName').value;
+        let lastName = document.getElementById('lastName').value;
+        let mobile = document.getElementById('mobile').value;
+        let password = document.getElementById('password').value;
+
+        if(firstName.length===0){
+            errorToast('First Name is required')
+        }
+        else if(lastName.length===0){
+            errorToast('Last Name is required')
+        }
+        else if(mobile.length===0){
+            errorToast('Mobile is required')
+        }
+        else if(password.length===0){
+            errorToast('Password is required')
+        }
+        else{
+            showLoader();
+            let res=await axios.post("/user-update",{
+                first_name:firstName,
+                last_name:lastName,
+                mobile:mobile,
+                password:password
+            })
+            hideLoader();
+            if(res.status===200 && res.data['status']==='success'){
+                successToast(res.data['message']);
+                await getProfile();
+            }
+            else{
+                errorToast(res.data['message'])
+            }
+        }
+    }
+</script>
